ed992fe9182469dc24a3140d8285c2e63f5eb5bb
MariaMarusakova <maruska.marusakova@gmail.com>
2024-03-05 20:44:42 +0100

    validation, correc submit

--- app/page.js
+++ app/page.js
@@ -7,14 +7,25 @@ import { DevTool } from '@hookform/devtools'
 import moment from "moment";
 
 
-export default function Home() {
-  const { control, register, onChange, getValues, watch } = useForm();
+export default function Home({ formData }) {
+  const { control, register, onChange, getValues, watch, handleSubmit, formState } = useForm();
+  const { errors } = formState;
+
   const enteredValues = watch();
 
   let calculatedDuration = moment();
   const [days, setDays] = useState(-1);
   const [years, setYears] = useState(-1);
   const [months, setMonths] = useState(-1);
+
+  const onSubmit = (data) => {
+    console.log("----------------------------------------");
+    console.log("Form Submitted", data);    
+    ageCalculator(enteredValues);
+    console.log("Entered Values", enteredValues);
+
+    console.log("----------------------------------------");
+  }
 
   const ageCalculator = (enteredValues, days, months, years) => {
     const enteredDate = new Date(enteredValues.year, enteredValues.month - 1, enteredValues.day);
@@ -34,39 +45,57 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="bg-white w-80 h-96 rounded-3xl rounded-br-[90px] md:w-1/3 md:h-[500px] md:rounded-br-[150px] block">
-        <form>
+      <div className="bg-white w-80 h-96 rounded-3xl rounded-br-[90px] md:w-2/5 md:h-[500px] md:rounded-br-[150px] block">
+        <form onSubmit={handleSubmit(onSubmit)} noValidate>
           <div className="mb-10 flex m-10">
             <div>
-              <label className="font-[600] text-xs text-[hsl(0,1%,44%)] tracking-widest">DAY</label>
-              <input name="day" placeholder="DD" className="flex h-10 w-16 md:h-12 md:w-24 rounded-md border-2 px-4 py-1.5 text-lg ring-offset-background focus-visible:outline-none 
-				focus-visible:border-purple-600 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 mr-2 md:mr-5" {...register("day", { onChange: () => onChange })}></input>
+              <label className={errors?.day?.message ? ["font-[600] text-xs text-red-700 tracking-widest"] : "font-[600] text-xs text-[hsl(0,1%,44%)] tracking-widest"}>DAY</label>
+              <input id="day" placeholder="DD" className="flex h-10 w-16 md:h-12 md:w-28 rounded-md border-2 px-4 py-1.5 text-lg ring-offset-background focus-visible:outline-none 
+				focus-visible:border-purple-600 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 mr-2 md:mr-5" type="text"
+                {...register("day", { required: "This field is required",  
+                validate: (value) =>
+                (value < 32 & value > 0) || "Must be a valid day" , onChange: () => onChange })}>
+              </input>
+              <p className='text-red-700 text-xs'>{errors.day?.message}</p>
+
             </div>
             <div>
-              <label className="font-[600] text-xs text-[hsl(0,1%,44%)] tracking-widest">MONTH</label>
-              <input name="month" placeholder="MM" className="flex h-10 w-16 md:h-12 md:w-24 rounded-md border-2 px-4 py-1.5 text-lg ring-offset-background focus-visible:outline-none 
-				focus-visible:border-purple-600 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 mr-4 md:mr-5" {...register("month", { onChange: () => onChange })}></input>
+              <label className={errors?.month?.message ? ["font-[600] text-xs text-red-700 tracking-widest"] : "font-[600] text-xs text-[hsl(0,1%,44%)] tracking-widest"}>MONTH</label>
+              <input name="month" placeholder="MM" className="flex h-10 w-16 md:h-12 md:w-28 rounded-md border-2 px-4 py-1.5 text-lg ring-offset-background focus-visible:outline-none 
+				focus-visible:border-purple-600 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 mr-4 md:mr-5"
+                {...register("month", { required: "This field is required",  validate: (value) =>
+                (value < 13 & value > 0) || "Must be a valid month", onChange: () => onChange })}></input>
+              <p className='text-red-700 text-xs'>{errors.month?.message}</p>
+
             </div><div>
-              <label className="font-[600] text-xs text-[hsl(0,1%,44%)] tracking-widest">YEAR</label>
+              <label className={errors?.year?.message ? ["font-[600] text-xs text-red-700 tracking-widest"] : "font-[600] text-xs text-[hsl(0,1%,44%)] tracking-widest"}>YEAR</label>
               <input name="year" placeholder="YYYY" className="flex h-10 w-20 md:h-12 md:w-36 rounded-md border-2 px-4 py-1.5 text-lg ring-offset-background focus-visible:outline-none 
-				focus-visible:border-purple-600 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50" {...register("year", { onChange: () => onChange })}></input>
+				focus-visible:border-purple-600 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+                {...register("year", { required: "This field is required", onChange: () => onChange, 
+                validate: (value) =>
+                value < 2025 || "Must be in the past" })}></input>
+              <p className='text-red-700 text-xs'>{errors.year?.message}</p>
+
             </div>
           </div>
+
+
+
+          <div className="md:mt-10 mt-10">
+            <hr className="bg-[hsl(0,0%,86%)] h-0.5 m-10 md:mt-10 md:ml-12 md:mr-16 md:mb-10" />
+            <button type="submit" className="-mt-16 md:-mt-20 scale-75 md:scale-100 h-15 w-15 md:ml-[550px] ml-[130px] flex md:h-20 md:w-20 bg-[hsl(259,100%,65%)] 
+        p-[5px] md:p-4 rounded-full hover:bg-black">
+              <svg className="scale-[55%] md:scale-100" xmlns="http://www.w3.org/2000/svg" width="46" height="44" viewBox="0 0 46 44"><g fill="none" stroke="#FFF" stroke-width="2">
+                <path d="M1 22.019C8.333 21.686 23 25.616 23 44M23 44V0M45 22.019C37.667 21.686 23 25.616 23 44" /></g></svg><span></span>
+            </button>
+          </div>
+
         </form>
 
-        <div className="md:mt-10 mt-10">
-          <hr className="bg-[hsl(0,0%,86%)] h-0.5 m-10 md:mt-10 md:ml-12 md:mr-16 md:mb-10" />
-          <button type="button" onClick={() => ageCalculator(enteredValues)} className="-mt-16 md:-mt-20 scale-75 md:scale-100 h-15 w-15 md:ml-[450px] ml-[130px] flex md:h-20 md:w-20 bg-[hsl(259,100%,65%)] 
-        p-[5px] md:p-4 rounded-full hover:bg-black">
-            <svg className="scale-[55%] md:scale-100" xmlns="http://www.w3.org/2000/svg" width="46" height="44" viewBox="0 0 46 44"><g fill="none" stroke="#FFF" stroke-width="2">
-              <path d="M1 22.019C8.333 21.686 23 25.616 23 44M23 44V0M45 22.019C37.667 21.686 23 25.616 23 44" /></g></svg><span></span>
-          </button>
-        </div>
-
         <div className="ml-5 md:ml-10 mt-6 md:mt-6">
-          <div className="font-[800] italic text-5xl md:text-7xl text-violet-600">{(years === -1) ? '--' : years} <span className="text-black">years</span></div>
-          <div className="font-[800] italic text-5xl md:text-7xl  text-violet-600">{(months === -1) ? '--' : months} <span className="text-black">months</span></div>
-          <div className="font-[800] italic text-5xl md:text-7xl text-violet-600">{(days === -1) ? '--' : days} <span className="text-black">days</span></div>
+          <div className="font-[800] italic text-5xl md:text-7xl text-violet-600">{(years === -1 || !years) ? '--' : years} <span className="text-black">years</span></div>
+          <div className="font-[800] italic text-5xl md:text-7xl  text-violet-600">{(months === -1 || !months) ? '--' : months} <span className="text-black">months</span></div>
+          <div className="font-[800] italic text-5xl md:text-7xl text-violet-600">{(days === -1 || !days) ? '--' : days} <span className="text-black">days</span></div>
         </div>
       </div>
 
